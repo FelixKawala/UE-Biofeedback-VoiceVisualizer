07e742726d147586e18a56916a3086e22c7ccd3f
fel2225s <Felix.Kawala@stud.hs-coburg.de>
2022-08-10 19:55:22 +0200

    Added new world, outline and optimized performance Also changed sound player to metasounds. (Maybe some other things too. I should commit more often.)

--- Source/Suko/RESTfunctionLibraray.h
+++ Source/Suko/RESTfunctionLibraray.h
@@ -11,7 +11,7 @@
  *
  */
 UCLASS()
-class ADUID_API URESTfunctionLibraray : public UBlueprintFunctionLibrary
+class SUKO_API URESTfunctionLibraray : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
